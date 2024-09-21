79b39cfe11962cca1c5f494420803295171bc18e
mu24081999 <116753849+mu24081999@users.noreply.github.com>
2024-09-21 18:54:17 +0500

    updated

--- src/config/WebServices.js
+++ src/config/WebServices.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://13.60.156.232:2001/api";
+// export const BASE_URL = "http://13.60.156.232:2001/api";
+export const BASE_URL = "https://cvj0m9w7-2001.inc1.devtunnels.ms/api/";
